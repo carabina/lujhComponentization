8c80189c3e6fbedd39171688748008538ae41a36
daniulaolu <287929070@qq.com>
2018-03-09 18:34:27 +0800

    eew

---
+++ lujhComponentization/Main/Pepole.h
@@ -0,0 +1,13 @@
+//
+//  Pepole.h
+//  lujhComponentizationExample
+//
+//  Created by lujh on 2018/3/9.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pepole : NSObject
+
+@end
---
+++ lujhComponentization/Main/Pepole.m
@@ -0,0 +1,13 @@
+//
+//  Pepole.m
+//  lujhComponentizationExample
+//
+//  Created by lujh on 2018/3/9.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import "Pepole.h"
+
+@implementation Pepole
+
+@end
